c8af20b5e54d7d0f327320587dd44cf608f0a51a
carlos <363465939@qq.com>
2019-01-27 20:47:56 +0800

    Add WechatHook.

--- app/src/main/java/com/carlos/cposed/AlipayHook.kt
+++ app/src/main/java/com/carlos/cposed/AlipayHook.kt
@@ -7,9 +7,42 @@ import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 /**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *            佛曰:
+ *                   写字楼里写字间，写字间里程序员；
+ *                   程序人员写程序，又拿程序换酒钱。
+ *                   酒醒只在网上坐，酒醉还来网下眠；
+ *                   酒醉酒醒日复日，网上网下年复年。
+ *                   但愿老死电脑间，不愿鞠躬老板前；
+ *                   奔驰宝马贵者趣，公交自行程序员。
+ *                   别人笑我忒疯癫，我笑自己命太贱；
+ *                   不见满街漂亮妹，哪个归得程序员？
+*/
+
+/**
  * Created by Carlos on 2019/1/25.
- * Change total money and yesterday profit in wealth tab.
- * Test in Alipay version 10.1.55
+ * Change the total money and yesterday profit in wealth tab.
+ * Test in Alipay version 10.1.55.
+ * Refer to artic:https://juejin.im/post/5c4531c451882524ff640bbc.
  */
 class AlipayHook : IXposedHookLoadPackage {
 
@@ -18,14 +51,12 @@ class AlipayHook : IXposedHookLoadPackage {
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (packageName == lpparam.packageName) {
-            XposedBridge.log("${TAG}load Alipay")
+            XposedBridge.log("${TAG}Load Alipay app.")
             val classLoader = lpparam.classLoader
             val hookClass = classLoader.loadClass("com.alipay.android.render.engine.viewbiz.AssetsHeaderV2View")
             val assetsCardModel = classLoader.loadClass("com.alipay.android.render.engine.model.AssetsCardModel")
 
             dumpClass(hookClass)
-
-            XposedBridge.log("${TAG}start hook")
 
             if (hookClass == null) return
             XposedHelpers.findAndHookMethod(
@@ -38,19 +69,15 @@ class AlipayHook : IXposedHookLoadPackage {
                 object : XC_MethodHook() {
                     override fun beforeHookedMethod(param: MethodHookParam) {
                         super.beforeHookedMethod(param)
-                        XposedBridge.log("this is dividing line.")
-                        XposedBridge.log("--------------------------------------------")
-                        XposedBridge.log("start change data")
+                        XposedBridge.log("This is dividing line.")
+                        XposedBridge.log("----------------------------------------------")
+                        XposedBridge.log("Start to change data.")
                         val data = param.args[0]
-                        XposedBridge.log( "${TAG}latestTotalView:"+data.javaClass.getField("latestTotalView").get(data).toString())
-                        XposedBridge.log("${TAG}totalYesterdayProfitView:"+data.javaClass.getField("totalYesterdayProfitView").get(data).toString())
-                        XposedBridge.log("${TAG}assetDesc:"+data.javaClass.getField("assetDesc").get(data))
-                        XposedBridge.log("${TAG}ext:"+data.javaClass.getField("ext").get(data))
-                        XposedBridge.log("${TAG}followAction:"+data.javaClass.getField("followAction").get(data))
-                        XposedBridge.log("${TAG}userPic:"+data.javaClass.getField("userPic").get(data))
+                        XposedBridge.log( "${TAG}data:"+data.javaClass.getField("latestTotalView").get(data).toString())
+                        XposedBridge.log("${TAG}data:"+data.javaClass.getField("totalYesterdayProfitView").get(data).toString())
 
-                        data.javaClass.getField("latestTotalView").set(data, "1000000.88")
-                        data.javaClass.getField("totalYesterdayProfitView").set(data, "983.25")
+                        data.javaClass.getField("latestTotalView").set(data, "36162848.69")
+                        data.javaClass.getField("totalYesterdayProfitView").set(data, "1003.81")
 
                     }
                 })
@@ -62,19 +89,19 @@ class AlipayHook : IXposedHookLoadPackage {
      * Pring all fileds,methods and classes in class.
      */
     private fun dumpClass(cClass: Class<*>) {
-        XposedBridge.log("${TAG}Fileds")
+        XposedBridge.log("${TAG}Fileds:")
         val cFileds = cClass.declaredFields
         for (filed in cFileds) {
             XposedBridge.log(TAG +filed.toString())
         }
 
-        XposedBridge.log("${TAG}Methods")
+        XposedBridge.log("${TAG}Methods:")
         val cMethods = cClass.declaredMethods
         for (method in cMethods) {
             XposedBridge.log(TAG + method.toString())
         }
 
-        XposedBridge.log("${TAG}Classes")
+        XposedBridge.log("${TAG}Classes:")
         val childClass = cClass.declaredClasses
         for (cChildClass in childClass) {
             XposedBridge.log(TAG +cChildClass.toString())
---
+++ app/src/main/java/com/carlos/cposed/WechatHook.kt
@@ -0,0 +1,125 @@
+package com.carlos.cposed
+
+import android.widget.TextView
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+/**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *            佛曰:
+ *                   写字楼里写字间，写字间里程序员；
+ *                   程序人员写程序，又拿程序换酒钱。
+ *                   酒醒只在网上坐，酒醉还来网下眠；
+ *                   酒醉酒醒日复日，网上网下年复年。
+ *                   但愿老死电脑间，不愿鞠躬老板前；
+ *                   奔驰宝马贵者趣，公交自行程序员。
+ *                   别人笑我忒疯癫，我笑自己命太贱；
+ *                   不见满街漂亮妹，哪个归得程序员？
+ */
+
+/**
+ * Created by Carlos on 2019/1/27.
+ * Change the change and chang wallet in change page.
+ * Test in Wechat version 6.7.3.
+ */
+class WechatHook : IXposedHookLoadPackage {
+
+    private val packageName = "com.tencent.mm"
+    private val TAG = "WechatHook-"
+
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (packageName == lpparam.packageName) {
+            XposedBridge.log("${TAG}Load Wechat app.")
+
+            val classLoader = lpparam.classLoader
+            val hookClass =
+                classLoader.loadClass("com.tencent.mm.plugin.wallet.balance.ui.WalletBalanceManagerUI") ?: return
+
+            dumpClass(hookClass)
+
+            getData(hookClass)
+
+            XposedHelpers.findAndHookMethod(hookClass, "aZ", object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    XposedBridge.log("This is dividing line.")
+                    XposedBridge.log("----------------------------------------------")
+                    XposedBridge.log("Start to change data.")
+                    var textview = XposedHelpers.getObjectField(param.thisObject, "qha") as TextView
+                    textview.text = "67348723.06"
+                    textview = XposedHelpers.getObjectField(param.thisObject, "qhi") as TextView
+                    XposedBridge.log("${TAG}data:" + textview.text)
+                    textview.text = "-6.66"
+                    super.afterHookedMethod(param)
+                }
+            })
+        }
+    }
+
+    private fun getData(hookClass: Class<*>) {
+        XposedHelpers.findAndHookMethod(hookClass, "onResume", object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                XposedBridge.log("This is dividing line.")
+                XposedBridge.log("----------------------------------------------")
+                XposedBridge.log("Start to get data")
+                var textview = XposedHelpers.getObjectField(param.thisObject, "qgx") as TextView
+                XposedBridge.log("${TAG}data:" + textview.text)
+                // This is amount of money.
+                textview = XposedHelpers.getObjectField(param.thisObject, "qha") as TextView
+                XposedBridge.log("${TAG}data:" + textview.text)
+                textview = XposedHelpers.getObjectField(param.thisObject, "qhe") as TextView
+                XposedBridge.log("${TAG}data:" + textview.text)
+                textview = XposedHelpers.getObjectField(param.thisObject, "qhh") as TextView
+                XposedBridge.log("${TAG}data:" + textview.text)
+                // This is amount of money pass .
+                textview = XposedHelpers.getObjectField(param.thisObject, "qhi") as TextView
+                XposedBridge.log("${TAG}data:" + textview.text)
+                super.afterHookedMethod(param)
+            }
+        })
+    }
+
+
+    private fun dumpClass(cClass: Class<*>) {
+        XposedBridge.log("${TAG}Fileds")
+        val cFileds = cClass.declaredFields
+        for (filed in cFileds) {
+            XposedBridge.log(TAG + filed.toString())
+        }
+
+        XposedBridge.log("${TAG}Methods")
+
+        val cMethods = cClass.declaredMethods
+        for (method in cMethods) {
+            XposedBridge.log(TAG + method.toString())
+        }
+
+        XposedBridge.log("${TAG}Classes")
+
+        val childClass = cClass.declaredClasses
+        for (cChildClass in childClass) {
+            XposedBridge.log(TAG + cChildClass.toString())
+        }
+    }
+}
